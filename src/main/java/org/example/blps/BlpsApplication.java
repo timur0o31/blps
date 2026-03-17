4656f602ec637f005ed7315bc747c7de1f69a6c1
Тимур Альметов <timuralmetov@MacBook-Pro-Timur-2.local>
2026-03-17 23:06:20 +0300

    init

---
+++ src/main/java/org/example/blps/BlpsApplication.java
@@ -0,0 +1,13 @@
+package org.example.blps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/blps/BlpsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
