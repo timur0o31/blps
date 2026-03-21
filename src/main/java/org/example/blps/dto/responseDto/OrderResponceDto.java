b19b6593a9e8c3f086eafe51f2f4c2a39a84338f
timur0o31 <almetovtimur2005@gmail.com>
2026-03-21 15:04:10 +0300

    add OrderStatusRequestDto

--- src/main/java/org/example/blps/dto/responseDto/OrderResponceDto.java
+++
@@ -1,9 +0,0 @@
-package org.example.blps.dto.responseDto;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
-
-@Setter
-public record OrderResponceDto {
-}
---
+++ src/main/java/org/example/blps/dto/responseDto/OrderResponseDto.java
@@ -0,0 +1,7 @@
+package org.example.blps.dto.responseDto;
+import org.example.blps.status.OrderStatus;
+
+
+public record OrderResponceDto(OrderStatus orderStatus) {
+
+}
