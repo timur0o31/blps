6813683083f0bccc460fadb115244998e6cd3383
timur0o31 <almetovtimur2005@gmail.com>
2026-03-31 15:52:19 +0300

    fix pom.xml

--- src/main/java/org/example/blps/service/CourierService.java
+++ src/main/java/org/example/blps/service/CourierService.java
@@ -1,24 +1,24 @@
-//package org.example.blps.service;
-//import org.example.blps.dto.requestDto.CourierRequestDto;
-//import org.example.blps.mapper.CourierMapper;
-//import org.example.blps.repository.CourierRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class CourierService {
-//
-//    private final CourierRepository courierRepository;
-//    private final CourierMapper courierMapper;
-//
-//    @Autowired
-//    public CourierService(CourierRepository courierRepository, CourierMapper courierMapper) {
-//        this.courierRepository = courierRepository;
-//        this.courierMapper = courierMapper;
-//    }
-//
-//    public void addCourier(CourierRequestDto courierRequestDto) {
-//        courierRepository.save(courierMapper.fromDtoToEntity(courierRequestDto));
-//    }
-//}
-//
+package org.example.blps.service;
+import org.example.blps.dto.requestDto.CourierRequestDto;
+import org.example.blps.mapper.CourierMapper;
+import org.example.blps.repository.CourierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourierService {
+
+    private final CourierRepository courierRepository;
+    private final CourierMapper courierMapper;
+
+    @Autowired
+    public CourierService(CourierRepository courierRepository, CourierMapper courierMapper) {
+        this.courierRepository = courierRepository;
+        this.courierMapper = courierMapper;
+    }
+
+    public void addCourier(CourierRequestDto courierRequestDto) {
+        courierRepository.save(courierMapper.fromDtoToEntity(courierRequestDto));
+    }
+}
+
