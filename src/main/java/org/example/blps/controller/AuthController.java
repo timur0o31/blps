b3ef0856946c80315b0640b920e83518efa7ef65
Данила Солодовников <danilasolodovnikov@MacBook-Air-Danila-2.local>
2026-04-04 15:08:37 +0300

    small fix

--- src/main/java/org/example/blps/controller/AuthController.java
+++ src/main/java/org/example/blps/controller/AuthController.java
@@ -3,7 +3,6 @@ import org.example.blps.dto.responseDto.JwtAuthificationResponceDto;
 import org.example.blps.dto.requestDto.UserCredentialsRequestDto;
 import org.example.blps.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
